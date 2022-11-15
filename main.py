aa7962f6cd6098f7d17e3c73aceb5961858d669d
VENKAT1923 <118198477+VENKAT1923@users.noreply.github.com>
2022-11-15 08:04:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+lee = turtle.Turtle()
+
+def square():
+  lee.forward(100)
+  lee.right(90)
+  lee.forward(100)
+  lee.right(90)
+  lee.forward(100)
+  lee.right(90)
+  lee.forward(100)
